8c2211b699ee142cbe524177af3dfc111c415dc6
Omochin <omochin7@gmail.com>
2019-02-26 21:27:58 +0900

    Add DEFAULT_PAGINATION env

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ VERSION = '1'
 
 #DAMMY_TWITTER_ID = os.environ.get('DAMMY_TWITTER_ID', 'kx6txew2')
 
-DEFAULT_PAGINATION = 50
+DEFAULT_PAGINATION = int(os.environ.get('DEFAULT_PAGINATION', 30))
 
 DEFAULT_CATEGORY = 'misc'
 DEFAULT_SEARCH_QUERY = '%20exclude%3Aretweets&src=typd&lang=ja&count=100'
